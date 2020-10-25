4f0f3f51ffd388ef265347bc8463d040f2070fc2
Mikhail Shardin <kriptonit1988@gmail.com>
2020-10-25 23:04:48 +0500

    Add config

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGODB: 'mongodb://merng-tutorial:rRpNkluHKywvBY5K@cluster0-shard-00-00.lc1rr.mongodb.net:27017,cluster0-shard-00-01.lc1rr.mongodb.net:27017,cluster0-shard-00-02.lc1rr.mongodb.net:27017/merng?ssl=true&replicaSet=atlas-r5vf4e-shard-0&authSource=admin&retryWrites=true&w=majority',
+  SECRET_KEY: 'secret key for token'
+};
